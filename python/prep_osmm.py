9f2fdb43a3f4e5946176cf57d3cfbbf6f9b54aec
Matt Walker <walkermatt@longwayaround.org.uk>
2011-08-30 13:28:00 +0100

    Updated prep_osmm class in prep_osmm.py to include logic for building comma seperated lists for theme, descriptiveGroup and descriptiveTerm and writing the 'corrected' orientation to an attribute called orientDeg. Added prep_osmm_qgis class to prep_osmm.py which sub-classes prep_osmm and includes the QGIS specific logic in preposmm4ogr_qgis.py but using lxml instead of SAX. Updated osmm_topo_shape_qgis.gfs to use the orientDeg attribute.
    To use the new prep_osmm_qgis class to prepare features use the following prep_cmd:
    
    prep_cmd=python prepgml4ogr.py $file_path prep_osmm.prep_osmm_qgis

--- python/prep_osmm.py
+++ python/prep_osmm.py
@@ -1,4 +1,6 @@
+import os
 from lxml import etree
+from timer import Timer
 
 class prep_osmm():
 
@@ -11,6 +13,7 @@ class prep_osmm():
             'TopographicLine',
             'TopographicPoint'
         ]
+        self.list_seperator = ', '
 
     def get_feat_types(self):
         return self.feat_types
@@ -19,18 +22,105 @@ class prep_osmm():
 
         # Parse the xml string into something useful
         feat_elm = etree.fromstring(feat_str)
+        feat_elm = self._prepare_feat_elm(feat_elm)
+
+        return etree.tostring(feat_elm, encoding="UTF-8", pretty_print=False)
+
+    def _prepare_feat_elm(self, feat_elm):
+
+        feat_elm = self._add_fid_elm(feat_elm)
+        feat_elm = self._add_orientation_degree_elms(feat_elm)
+        feat_elm = self._add_lists_elms(feat_elm)
+        
+        return feat_elm
+
+    def _add_fid_elm(self, feat_elm):
 
         # Create an element with the fid
         elm = etree.SubElement(feat_elm, "fid")
         elm.text = feat_elm.get('fid')
 
+        return feat_elm
+        
+
+    def _add_orientation_degree_elms(self, feat_elm):
+
         # Correct any orientation values to be a
         # tenth of their original value
         orientation_elms = feat_elm.xpath('//orientation')
         for elm in orientation_elms:
-            # Update the orientation to be orientation/10
+            # Add a new orientDeg element as a child to the
+            # the orientation elm to be orientation/10
             # (this applies integer division which is fine in
             # this instance as we are not concerned with the decimals)
-            elm.text = str(int(elm.text)/10)
-            
-        return etree.tostring(feat_elm, encoding="UTF-8")
+            degree_elm = etree.SubElement(elm.getparent(), "orientDeg")
+            degree_elm.text = str(int(elm.text)/10)
+
+        return feat_elm
+
+    def _add_lists_elms(self, feat_elm):
+
+        feat_elm = self._create_list_of_terms(feat_elm, 'theme')
+        feat_elm = self._create_list_of_terms(feat_elm, 'descriptiveGroup')
+        feat_elm = self._create_list_of_terms(feat_elm, 'descriptiveTerm')
+
+        return feat_elm
+        
+    def _create_list_of_terms(self, feat_elm, name):
+        text_list = feat_elm.xpath('//%s/text()' % name)
+        if len(text_list):
+            elm = etree.SubElement(feat_elm, "%ss" % name)
+            elm.text = self.list_seperator.join(text_list)
+        return feat_elm
+
+class prep_osmm_qgis(prep_osmm):
+
+    def __init__ (self):
+        prep_osmm.__init__(self)
+
+        # AC - define the fonts to use when rendering in QGIS
+        if os.name is 'posix': 
+            # Will probably need different font names
+            self.fonts = ('Garamond','Arial','Roman','ScriptC')  
+        elif os.name is 'nt':
+           # Ordnance Survey use 
+           #   'Lutheran','Normal','Light Roman','Suppressed text'
+           self.fonts = ('GothicE','Monospac821 BT','Consolas','ScriptC','Arial Narrow') 
+        elif os.name is 'mac':
+           # Will probably need different font name
+           self.fonts = ('Garamond','Arial','Roman','ScriptC') 
+     
+        # AC - the possible text placement positions used by QGIS   
+        self.anchorPosition = ('Bottom Left','Left','Top Left','Bottom','Over',
+                               'Top', 'Bottom Right','Right','Top Right')
+
+    def _prepare_feat_elm(self, feat_elm):
+
+        feat_elm = prep_osmm._prepare_feat_elm(self, feat_elm)
+        feat_elm = self._add_qgis_elms(feat_elm)
+
+        return feat_elm
+
+    def _add_qgis_elms(self, feat_elm):
+        
+        if feat_elm.tag == 'CartographicText':
+            text_render_elm = feat_elm.xpath('//textRendering')[0]
+
+            anchor_pos = int(text_render_elm.xpath('./anchorPosition/text()')[0])
+            try:
+                anchor_pos = self.anchorPosition[anchor_pos]
+            except:
+                anchor_pos = 4
+            elm = etree.SubElement(text_render_elm, 'qAnchorPos')
+            elm.text = anchor_pos
+
+            font = int(text_render_elm.xpath('./font/text()')[0])
+            try:
+                font = self.fonts[font]
+            except:
+                font = 'unknown font (%s)' % str(font)
+            elm = etree.SubElement(text_render_elm, 'qFont')
+            elm.text = font
+
+        return feat_elm
+
