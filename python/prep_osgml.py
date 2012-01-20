bc34c23aa9464c7e1ed53dacb35122727d11f429
Matt Walker <walkermatt@longwayaround.org.uk>
2012-01-20 11:56:38 +0000

    Added support for loading OS MasterMap ITN and OS VectorMap Local data
    Moved all .gfs files (used by OGR to map elements in the GML to simple feature attributes) to the gfs directory so reduce the clutter in the python directory

--- python/prep_osgml.py
+++ python/prep_osgml.py
@@ -1,19 +1,19 @@
+"""
+A collection of classes used to manipulate Ordnance Survey GB GML data, used with prepgml4ogr.py.
+"""
 import os
 from lxml import etree
 
-class prep_osmm():
+class prep_osgml():
+    """
+    Base class that provides the main interface methods `prepare_feature` and `get_feat_types`
+    and performs basic manipulation such as exposing the fid, adding and element containing
+    the filename of the source and adding an element with the orientation in degrees.
 
+    """
     def __init__ (self, inputfile):
         self.inputfile = inputfile
-        self.feat_types = [
-            'BoundaryLine',
-            'CartographicSymbol',
-            'CartographicText',
-            'TopographicArea',
-            'TopographicLine',
-            'TopographicPoint'
-        ]
-        self.list_seperator = ', '
+        self.feat_types = []
 
     def get_feat_types(self):
         return self.feat_types
@@ -23,15 +23,14 @@ class prep_osmm():
         # Parse the xml string into something useful
         feat_elm = etree.fromstring(feat_str)
         feat_elm = self._prepare_feat_elm(feat_elm)
-
-        return etree.tostring(feat_elm, encoding="UTF-8", pretty_print=False).decode('utf_8');
+        
+        return etree.tostring(feat_elm, encoding='UTF-8', pretty_print=True).decode('utf_8');
 
     def _prepare_feat_elm(self, feat_elm):
 
         feat_elm = self._add_fid_elm(feat_elm)
         feat_elm = self._add_filename_elm(feat_elm)
         feat_elm = self._add_orientation_degree_elms(feat_elm)
-        feat_elm = self._add_lists_elms(feat_elm)
         
         return feat_elm
 
@@ -66,6 +65,47 @@ class prep_osmm():
 
         return feat_elm
 
+class prep_vml(prep_osgml):
+    """
+    Preperation class for OS VectorMap Local features.
+
+    """
+    def __init__ (self, inputfile):
+        prep_osgml.__init__(self, inputfile)
+        self.feat_types = [
+            'Text',
+            'VectorMapPoint',
+            'Line',
+            'RoadCLine',
+            'Area'
+        ]
+
+class prep_osmm(prep_osgml):
+    """
+    Preperation class for OS MasterMap features which in addition to the work performed by
+    `prep_osgml` adds `themes`, `descriptiveGroups` and `descriptiveTerms` elements containing
+    a delimited string of the attributes that can appear multiple times.
+
+    """
+    def __init__ (self, inputfile):
+        prep_osgml.__init__(self, inputfile)
+        self.feat_types = [
+            'BoundaryLine',
+            'CartographicSymbol',
+            'CartographicText',
+            'TopographicArea',
+            'TopographicLine',
+            'TopographicPoint'
+        ]
+        self.list_seperator = ', '
+
+    def _prepare_feat_elm(self, feat_elm):
+
+        feat_elm = prep_osgml._prepare_feat_elm(self, feat_elm)
+        feat_elm = self._add_lists_elms(feat_elm)
+        
+        return feat_elm
+
     def _add_lists_elms(self, feat_elm):
 
         feat_elm = self._create_list_of_terms(feat_elm, 'theme')
@@ -82,9 +122,14 @@ class prep_osmm():
         return feat_elm
 
 class prep_osmm_qgis(prep_osmm):
+    """
+    Preperation class for OS MasterMap features which in addition to the work performed by
+    `prep_osmm` adds QGIS specific label attributes such as `qFont` and `aAnchorPos`.
 
-    def __init__ (self):
-        prep_osmm.__init__(self)
+    """
+
+    def __init__ (self, filename):
+        prep_osmm.__init__(self, filename)
 
         # AC - define the fonts to use when rendering in QGIS
         if os.name is 'posix': 
@@ -132,3 +177,45 @@ class prep_osmm_qgis(prep_osmm):
 
         return feat_elm
 
+class prep_itn(prep_osgml):
+    """
+    Preperation class for OS MasterMap ITN features.
+
+    """
+
+    def __init__ (self, filename):
+
+        prep_osgml.__init__(self, filename)
+
+        self.feat_types = [
+            'FerryLink',
+            'FerryNode',
+            'InformationPoint',
+            'Road',
+            'RoadLinkInformation',
+            'RoadRouteInformation'
+        ]
+
+    def _prepare_feat_elm(self, feat_elm):
+
+        feat_elm = prep_osgml._prepare_feat_elm(self, feat_elm)
+        feat_elm = self._expose_links(feat_elm)
+
+        return feat_elm
+
+    def _expose_links(self, feat_elm):
+        
+        link_list = feat_elm.xpath('//networkMember | //directedLink | //directedNode | //referenceToRoadLink')
+        for elm in link_list:
+            for name in elm.attrib:
+                value = elm.get(name)
+                if name == 'href':
+                    name = '%s_%s' % (elm.tag, 'ref')
+                    value = value[1:]
+                elif name == 'orientation':
+                    name = '%s_%s' % (elm.tag, name)
+                    value = '1' if value == '+' else '0'
+                sub_elm = etree.SubElement(elm, name)
+                sub_elm.text = value
+
+        return feat_elm
