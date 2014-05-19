734dad41b17198d308acf4529e7d09d21efe0a20
Matt Walker <walkermatt@longwayaround.org.uk>
2014-05-19 13:48:47 +0100

    Add SQL to create VML tables and indexes when loading via PGDump driver

---
+++ extras/ordnancesurvey/vml/pgdump/create-indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX "text_geom_idx" ON "vml"."text" USING GIST ("wkb_geometry");
+CREATE INDEX "vectormappoint_geom_idx" ON "vml"."vectormappoint" USING GIST ("wkb_geometry");
+CREATE INDEX "line_geom_idx" ON "vml"."line" USING GIST ("wkb_geometry");
+CREATE INDEX "roadcline_geom_idx" ON "vml"."roadcline" USING GIST ("wkb_geometry");
+CREATE INDEX "area_geom_idx" ON "vml"."area" USING GIST ("wkb_geometry");
---
+++ extras/ordnancesurvey/vml/pgdump/create-tables.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS "vml"."text" CASCADE;
+DELETE FROM geometry_columns WHERE f_table_name = 'text' AND f_table_schema = 'vml';
+
+CREATE TABLE "vml"."text" ( OGC_FID SERIAL, CONSTRAINT "text_pk" PRIMARY KEY (OGC_FID) );
+SELECT AddGeometryColumn('vml','text','wkb_geometry',27700,'POINT',2);
+ALTER TABLE "vml"."text" ADD COLUMN "fid" VARCHAR;
+ALTER TABLE "vml"."text" ADD COLUMN "featurecode" INTEGER;
+ALTER TABLE "vml"."text" ADD COLUMN "featuredescription" VARCHAR;
+ALTER TABLE "vml"."text" ADD COLUMN "anchorposition" INTEGER;
+ALTER TABLE "vml"."text" ADD COLUMN "font" INTEGER;
+ALTER TABLE "vml"."text" ADD COLUMN "height" FLOAT8;
+ALTER TABLE "vml"."text" ADD COLUMN "orientation" INTEGER;
+ALTER TABLE "vml"."text" ADD COLUMN "orientdeg" FLOAT8;
+ALTER TABLE "vml"."text" ADD COLUMN "textstring" VARCHAR;
+ALTER TABLE "vml"."text" ADD COLUMN "tile" VARCHAR;
+ALTER TABLE "vml"."text" ADD COLUMN "creationdate" DATE;
+
+DROP TABLE IF EXISTS "vml"."vectormappoint" CASCADE;
+DELETE FROM geometry_columns WHERE f_table_name = 'vectormappoint' AND f_table_schema = 'vml';
+
+CREATE TABLE "vml"."vectormappoint" ( OGC_FID SERIAL, CONSTRAINT "vectormappoint_pk" PRIMARY KEY (OGC_FID) );
+SELECT AddGeometryColumn('vml','vectormappoint','wkb_geometry',27700,'POINT',2);
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "fid" VARCHAR;
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "featurecode" INTEGER;
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "featuredescription" VARCHAR;
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "orientation" INTEGER;
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "orientdeg" FLOAT8;
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "tile" VARCHAR;
+ALTER TABLE "vml"."vectormappoint" ADD COLUMN "creationdate" DATE;
+
+DROP TABLE IF EXISTS "vml"."line" CASCADE;
+DELETE FROM geometry_columns WHERE f_table_name = 'line' AND f_table_schema = 'vml';
+
+CREATE TABLE "vml"."line" ( OGC_FID SERIAL, CONSTRAINT "line_pk" PRIMARY KEY (OGC_FID) );
+SELECT AddGeometryColumn('vml','line','wkb_geometry',27700,'LINESTRING',2);
+ALTER TABLE "vml"."line" ADD COLUMN "fid" VARCHAR;
+ALTER TABLE "vml"."line" ADD COLUMN "featurecode" INTEGER;
+ALTER TABLE "vml"."line" ADD COLUMN "featuredescription" VARCHAR;
+ALTER TABLE "vml"."line" ADD COLUMN "tile" VARCHAR;
+ALTER TABLE "vml"."line" ADD COLUMN "creationdate" DATE;
+
+DROP TABLE IF EXISTS "vml"."roadcline" CASCADE;
+DELETE FROM geometry_columns WHERE f_table_name = 'roadcline' AND f_table_schema = 'vml';
+
+CREATE TABLE "vml"."roadcline" ( OGC_FID SERIAL, CONSTRAINT "roadcline_pk" PRIMARY KEY (OGC_FID) );
+SELECT AddGeometryColumn('vml','roadcline','wkb_geometry',27700,'LINESTRING',2);
+ALTER TABLE "vml"."roadcline" ADD COLUMN "fid" VARCHAR;
+ALTER TABLE "vml"."roadcline" ADD COLUMN "featurecode" INTEGER;
+ALTER TABLE "vml"."roadcline" ADD COLUMN "featuredescription" VARCHAR;
+ALTER TABLE "vml"."roadcline" ADD COLUMN "roadnumber" VARCHAR;
+ALTER TABLE "vml"."roadcline" ADD COLUMN "roadname" VARCHAR;
+ALTER TABLE "vml"."roadcline" ADD COLUMN "tile" VARCHAR;
+ALTER TABLE "vml"."roadcline" ADD COLUMN "creationdate" DATE;
+
+DROP TABLE IF EXISTS "vml"."area" CASCADE;
+DELETE FROM geometry_columns WHERE f_table_name = 'area' AND f_table_schema = 'vml';
+
+CREATE TABLE "vml"."area" ( OGC_FID SERIAL, CONSTRAINT "area_pk" PRIMARY KEY (OGC_FID) );
+SELECT AddGeometryColumn('vml','area','wkb_geometry',27700,'POLYGON',2);
+ALTER TABLE "vml"."area" ADD COLUMN "fid" VARCHAR;
+ALTER TABLE "vml"."area" ADD COLUMN "featurecode" INTEGER;
+ALTER TABLE "vml"."area" ADD COLUMN "featuredescription" VARCHAR;
+ALTER TABLE "vml"."area" ADD COLUMN "tile" VARCHAR;
+ALTER TABLE "vml"."area" ADD COLUMN "creationdate" DATE;
