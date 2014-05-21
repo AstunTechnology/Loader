edeacbeb74da8f29d2aeb9f4247e2436507fb664
Matt Walker <walkermatt@longwayaround.org.uk>
2014-05-21 14:40:04 +0100

    Use LIKE clause of CREATE TABLE to create update and archive tables.
    Avoids using the CREATE TABLE foo AS SELECT hack for the archive tables
    and greatly reduces the SQL required for the update tables.

--- extras/ordnancesurvey/vml/updates/create-archive-tables.sql
+++ extras/ordnancesurvey/vml/updates/create-archive-tables.sql
@@ -1,10 +1,9 @@
 -- Create tables suitable for archiving superseded VML features. Assumes there
--- is a schema called vml_archive. It's expected that this script is ran once
--- following an initial load. The script apply-update.sql manages moving
+-- is a schema called vml_archive. The script apply-update.sql manages moving
 -- superseded features to the archive tables.
 
-CREATE TABLE vml_archive.area AS SELECT * FROM vml.area WHERE ogc_fid = -1;
-CREATE TABLE vml_archive.line AS SELECT * FROM vml.line WHERE ogc_fid = -1;
-CREATE TABLE vml_archive.roadcline AS SELECT * FROM vml.roadcline WHERE ogc_fid = -1;
-CREATE TABLE vml_archive.text AS SELECT * FROM vml.text WHERE ogc_fid = -1;
-CREATE TABLE vml_archive.vectormappoint AS SELECT * FROM vml.vectormappoint WHERE ogc_fid = -1;
+CREATE TABLE IF NOT EXISTS vml_archive.text (LIKE vml.text);
+CREATE TABLE IF NOT EXISTS vml_archive.vectormappoint (LIKE vml.vectormappoint);
+CREATE TABLE IF NOT EXISTS vml_archive.line (LIKE vml.line);
+CREATE TABLE IF NOT EXISTS vml_archive.roadcline (LIKE vml.roadcline);
+CREATE TABLE IF NOT EXISTS vml_archive.area (LIKE vml.area);
--- extras/ordnancesurvey/vml/updates/create-update-tables.sql
+++ extras/ordnancesurvey/vml/updates/create-update-tables.sql
@@ -1,73 +1,25 @@
 -- Create tables suitable for loading a VML update. Drops any existing VML
--- update tables. Assumes there is a schema called vml_update. It is expected
--- that this script is ran prior to an update being loaded.
+-- update tables. Assumes there is a schema called vml_update. The ogc_fid
+-- column is dropped to avoid issues with inserting into the main tables as
+-- ogc_fid is an auto generated SERIAL. It is expected that this script is ran
+-- prior to an update being loaded.
 
-DROP TABLE IF EXISTS "vml_update"."text" CASCADE;
-DELETE FROM geometry_columns WHERE f_table_name = 'text' AND f_table_schema = 'vml_update';
-CREATE TABLE vml_update.text
-(
-  fid character varying,
-  featurecode integer,
-  featuredescription character varying,
-  anchorposition integer,
-  font integer,
-  height double precision,
-  orientation integer,
-  orientdeg double precision,
-  textstring character varying,
-  tile character varying,
-  creationdate date
-);
-SELECT AddGeometryColumn('vml_update','text','wkb_geometry',27700,'POINT',2);
+DROP TABLE IF EXISTS vml_update.text CASCADE;
+CREATE TABLE vml_update.text (LIKE vml.text);
+ALTER TABLE vml_update.text DROP COLUMN ogc_fid;
 
-DROP TABLE IF EXISTS "vml_update"."vectormappoint" CASCADE;
-DELETE FROM geometry_columns WHERE f_table_name = 'vectormappoint' AND f_table_schema = 'vml_update';
-CREATE TABLE vml_update.vectormappoint
-(
-  fid character varying,
-  featurecode integer,
-  featuredescription character varying,
-  orientation integer,
-  orientdeg double precision,
-  tile character varying,
-  creationdate date
-);
-SELECT AddGeometryColumn('vml_update','vectormappoint','wkb_geometry',27700,'POINT',2);
+DROP TABLE IF EXISTS vml_update.vectormappoint CASCADE;
+CREATE TABLE vml_update.vectormappoint (LIKE vml.vectormappoint);
+ALTER TABLE vml_update.vectormappoint DROP COLUMN ogc_fid;
 
-DROP TABLE IF EXISTS "vml_update"."line" CASCADE;
-DELETE FROM geometry_columns WHERE f_table_name = 'line' AND f_table_schema = 'vml_update';
-CREATE TABLE vml_update.line
-(
-  fid character varying,
-  featurecode integer,
-  featuredescription character varying,
-  tile character varying,
-  creationdate date
-);
-SELECT AddGeometryColumn('vml_update','line','wkb_geometry',27700,'LINESTRING',2);
+DROP TABLE IF EXISTS vml_update.line CASCADE;
+CREATE TABLE vml_update.line (LIKE vml.line);
+ALTER TABLE vml_update.line DROP COLUMN ogc_fid;
 
-DROP TABLE IF EXISTS "vml_update"."roadcline" CASCADE;
-DELETE FROM geometry_columns WHERE f_table_name = 'roadcline' AND f_table_schema = 'vml_update';
-CREATE TABLE vml_update.roadcline
-(
-  fid character varying,
-  featurecode integer,
-  featuredescription character varying,
-  roadnumber character varying,
-  roadname character varying,
-  tile character varying,
-  creationdate date
-);
-SELECT AddGeometryColumn('vml_update','roadcline','wkb_geometry',27700,'LINESTRING',2);
+DROP TABLE IF EXISTS vml_update.roadcline CASCADE;
+CREATE TABLE vml_update.roadcline (LIKE vml.roadcline);
+ALTER TABLE vml_update.roadcline DROP COLUMN ogc_fid;
 
-DROP TABLE IF EXISTS "vml_update"."area" CASCADE;
-DELETE FROM geometry_columns WHERE f_table_name = 'area' AND f_table_schema = 'vml_update';
-CREATE TABLE vml_update.area
-(
-  fid character varying,
-  featurecode integer,
-  featuredescription character varying,
-  tile character varying,
-  creationdate date
-);
-SELECT AddGeometryColumn('vml_update','area','wkb_geometry',27700,'POLYGON',2);
+DROP TABLE IF EXISTS vml_update.area CASCADE;
+CREATE TABLE vml_update.area (LIKE vml.area);
+ALTER TABLE vml_update.area DROP COLUMN ogc_fid;
