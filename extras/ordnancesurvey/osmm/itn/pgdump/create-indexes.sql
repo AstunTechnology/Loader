e8d52bb7418320b2e80764d1bbefef27ad4116fe
Matt Walker <walkermatt@longwayaround.org.uk>
2014-12-19 16:06:11 +0000

    PGDump config for ITN

---
+++ extras/ordnancesurvey/osmm/itn/pgdump/create-indexes.sql
@@ -0,0 +1,7 @@
+CREATE INDEX "road_wkb_geometry_geom_idx" ON "osmm_itn"."road" USING GIST ("wkb_geometry");
+CREATE INDEX "roadlink_wkb_geometry_geom_idx" ON "osmm_itn"."roadlink" USING GIST ("wkb_geometry");
+CREATE INDEX "roadnode_wkb_geometry_geom_idx" ON "osmm_itn"."roadnode" USING GIST ("wkb_geometry");
+CREATE INDEX "ferrynode_wkb_geometry_geom_idx" ON "osmm_itn"."ferrynode" USING GIST ("wkb_geometry");
+CREATE INDEX "informationpoint_wkb_geometry_geom_idx" ON "osmm_itn"."informationpoint" USING GIST ("wkb_geometry");
+CREATE INDEX "roadlinkinformation_wkb_geometry_geom_idx" ON "osmm_itn"."roadlinkinformation" USING GIST ("wkb_geometry");
+CREATE INDEX "roadrouteinformation_wkb_geometry_geom_idx" ON "osmm_itn"."roadrouteinformation" USING GIST ("wkb_geometry");
