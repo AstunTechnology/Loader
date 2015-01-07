df762d05371b7510a6ddd419100a4d2fa7b22af8
Matt Walker <walkermatt@longwayaround.org.uk>
2015-01-07 14:35:54 +0000

    ITN cross reference views

--- extras/ordnancesurvey/osmm/itn/views.sql
+++ extras/ordnancesurvey/osmm/itn/views.sql
@@ -7,10 +7,53 @@ FROM
           unnest(networkmember_href) AS roadlink_fid
    FROM osmm_itn.road) AS a;
 
+-- Lookup between ferrylink and ferrynode on fid
+CREATE OR REPLACE VIEW osmm_itn.ferrylink_ferrynode AS
+SELECT ferrylink_fid,
+       replace(ferrynode_fid, '#', '') AS ferrynode_fid
+FROM
+  (SELECT fid AS ferrylink_fid,
+          unnest(directednode_href) AS ferrynode_fid
+   FROM osmm_itn.ferrylink) AS a;
+
+-- Lookup between ferryterminal and ferrynode on fid. A duplicate of
+-- ferryterminal_roadnode as ferryterminal.referencetonetwork_href references
+-- both ferrynode and roadnode
+CREATE OR REPLACE VIEW osmm_itn.ferryterminal_ferrynode AS
+SELECT ferryterminal_fid,
+       replace(ferrynode_fid, '#', '') AS ferrynode_fid
+FROM
+  (SELECT fid AS ferryterminal_fid,
+          unnest(referencetonetwork_href) AS ferrynode_fid
+   FROM osmm_itn.ferryterminal) AS a;
+
+-- Lookup between ferryterminal and roadnode on fid. A duplicate of
+-- ferryterminal_ferrynode as ferryterminal.referencetonetwork_href references
+-- both ferrynode and roadnode
+CREATE OR REPLACE VIEW osmm_itn.ferryterminal_roadnode AS
+SELECT ferryterminal_fid,
+       replace(roadnode_fid, '#', '') AS roadnode_fid
+FROM
+  (SELECT fid AS ferryterminal_fid,
+          unnest(referencetonetwork_href) AS roadnode_fid
+   FROM osmm_itn.ferryterminal) AS a;
+
+-- Lookup between roadrouteinformation and roadlink on fid including the order
+-- of each roadlink associated with a given roadrouteinformation row
+CREATE OR REPLACE VIEW osmm_itn.roadrouteinformation_roadlink AS
+SELECT roadrouteinformation_fid,
+       replace(roadlink_fid, '#', '') AS roadlink_fid,
+       roadlink_order
+FROM
+  (SELECT fid AS roadrouteinformation_fid,
+          unnest(directedlink_href) AS roadlink_fid,
+          generate_subscripts(directedlink_href, 1) AS roadlink_order
+   FROM osmm_itn.roadrouteinformation) AS a;
+
 -- Each roadlink with associated roadname(s) and fid of road in case more info
 -- is required
 CREATE OR REPLACE VIEW osmm_itn.roads AS
-SELECT array_to_string(road.roadname, ', ', '') AS roadname,
+SELECT array_to_string(road.roadname, ', ') AS roadname,
        road.fid AS road_fid,
        roadlink.*
 FROM osmm_itn.roadlink AS roadlink
