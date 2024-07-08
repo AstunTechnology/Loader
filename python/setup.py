f992f3ff60f408fcd84bf9e3d2d31eadb18189ad
Matt Walker <walkermatt@longwayaround.org.uk>
2024-07-08 16:00:15 +0100

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("../readme.md", "r") as fh:
-    long_description = fh.read()
-
 setup(name='Loader',
       version='1.3.0',
-      description='Scripts to convert classic layers to OGC layers',
-      url='',
-      author='Ian Turton',
-      author_email='ianturton@astuntechnology.com',
+      description='A loader for geographic data in GML',
+      url='https://github.com/AstunTechnology/Loader',
+      author='Matt Walker',
+      author_email='mattwalker@astuntechnology.com',
       license='MIT',
       packages=find_packages(),
-      long_description=long_description,
-      long_description_content_type="text/markdown",
+      long_description='A loader for geographic data in GML that needs some preparation before loading via ogr2ogr.',
+      long_description_content_type="text/plain",
       python_requires='>=3.6',
       install_requires=['lxml'],
       zip_safe=False)
